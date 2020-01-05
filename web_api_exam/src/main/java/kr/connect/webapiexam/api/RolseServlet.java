694a27b1c681be6df4c64c93c8b119fa22157617
Sung <seongsee@gmail.com>
2020-01-06 00:26:19 +0900

    Web API으로 GET 요청하면 Role 테이블의

---
+++ web_api_exam/src/main/java/kr/connect/webapiexam/api/RolseServlet.java
@@ -0,0 +1,57 @@
+package kr.connect.webapiexam.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kr.connect.webapiexam.dao.RoleDao;
+import kr.connect.webapiexam.dto.Role;
+
+/**
+ * Servlet implementation class RolseServlet
+ */
+@WebServlet("/Rolse")
+public class RolseServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RolseServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("application/json");
+		
+		RoleDao dao = new RoleDao();
+		
+		List<Role> list = dao.getRoles();
+		
+		/* JSON 문자열로 바꾸거나, JSON 문자열을 객체로 바꿔주는 객체 */
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		/* 파라미터로 list를 넘겨주면 해당 list가 JSON문자열로 바뀌어서 리턴 */
+		String json = objectMapper.writeValueAsString(list);
+		
+		PrintWriter out = response.getWriter();
+		out.println(json);
+		out.close();
+	}
+
+}
---
+++ web_api_exam/src/main/java/kr/connect/webapiexam/dao/RoleDao.java
@@ -0,0 +1,49 @@
+package kr.connect.webapiexam.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.connect.webapiexam.dto.Role;
+
+public class RoleDao {
+	private static String dburl = "jdbc:mysql://localhost:3306/connectdb?serverTimezone=Asia/Seoul&useSSL=false";
+	private static String dbUser = "connectuser";
+	private static String dbpasswd = "connect123!@#";
+	
+	public List<Role> getRoles() {
+		List<Role> list = new ArrayList<>();
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		String sql = "SELECT description, role_id FROM role order by role_id desc";
+		try (Connection conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+				PreparedStatement ps = conn.prepareStatement(sql)) {
+			
+			try (ResultSet rs = ps.executeQuery()) {
+				
+				while(rs.next()) {
+					String description = rs.getString(1);
+					int id = rs.getInt("role_id");
+					Role role = new Role(id, description);
+					list.add(role); //list에 반복할때마다 Role 인스턴스를 생성하여 list에 추가한다
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return list;
+	}
+}
